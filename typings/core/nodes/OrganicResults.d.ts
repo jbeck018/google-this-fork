a0c8a94765ea4a1ec06fca0313416c0a158e2aa3
Jacob Beck <jacob@parlor.io>
2023-03-02 07:41:27 -0700

    type update

--- typings/core/nodes/OrganicResults.d.ts
+++ typings/core/nodes/OrganicResults.d.ts
@@ -3,23 +3,21 @@ declare class OrganicResults {
     /**
      * @returns {{
      *   title: string;
+     *   subtitle: string;
      *   description: string;
      *   url: string;
      *   is_sponsored: boolean;
-     *   favicons: {
-     *     high_res: string;
-     *     low_res: string;
-     *   }
+    //  *   favicons: {
+    //  *     high_res: string;
+    //  *     low_res: string;
+    //  *   }
      *  }[]}
      */
     static parse($: any, parse_ads?: boolean, is_mobile?: boolean): {
         title: string;
+        subtitle: string;
         description: string;
         url: string;
         is_sponsored: boolean;
-        favicons: {
-            high_res: string;
-            low_res: string;
-        };
     }[];
 }
